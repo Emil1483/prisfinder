dae6cb3369ada26e3bb8b84cc635c942adead6e7
Emil Djupvik <emil14833@gmail.com>
2023-12-26 19:24:11 +0100

    add dot_env before creating redis connection

--- crawler/api/__init__.py
+++ crawler/api/__init__.py
@@ -1,7 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
 from src.services.redis_service import RedisService
 
 
+load_dotenv()
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 redis: RedisService = RedisService.from_url(REDIS_URL)
